a99409f34cd85f11207dca79acf96edc9e1bcda8
HOWARD LARRISON <development@daybreak.coffee>
2018-07-15 20:42:02 +0700

    Update mutationsigner.js

--- tests/mutationsigner.js
+++ tests/mutationsigner.js
@@ -1,7 +1,7 @@
 ﻿// Copyright (c) 2010-2018 The Bitcoin developers
 // Original code was distributed under the MIT software license.
 // Copyright (c) 2014-2018 TEDLab Sciences Ltd
-// Tedchain code distributed under the GPLv3 license, see COPYING file.
+// Tedchain code distributed under the GPLv3 license, see COPYING file
 
 var assert = require("assert");
 var bitcore = require("bitcore-lib");
